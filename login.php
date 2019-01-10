807155b7f8f77edad89765c5e5bc81fe962c1923
Administrator <1064693056@qq.com>
2019-01-10 10:48:49 +0800

    23333

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin : http://10.30.29.52:63341");
+header("Access-Control-Allow-Origin : http://10.30.29.53:63341");
 header("Access-Control-Allow-Credentials : true");
 header('Access-Control-Allow-Headers : Content-Type');
 //echo '{"userName":"NBThreeMan"}';
